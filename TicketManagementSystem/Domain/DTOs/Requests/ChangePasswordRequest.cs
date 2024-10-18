8940da9e4102abbbbe542c00172922dc69ef9e79
stephane.mouako <stephane.mouako@sioux.eu>
2024-10-18 15:36:10 +0200

    Redesigned page Change password

--- TicketManagementSystem/Domain/DTOs/Requests/ChangePasswordRequest.cs
+++ TicketManagementSystem/Domain/DTOs/Requests/ChangePasswordRequest.cs
@@ -6,9 +6,18 @@ public class ChangePasswordRequest
 {
     #region Public properties declaration
 
-    [Required] public string? ConfirmNewPassword { get; set; }
-    [Required] public string? CurrentPassword { get; set; }
-    [Required] public string? NewPassword { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare(nameof(NewPassword))]
+    public string? ConfirmNewPassword { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string? CurrentPassword { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string? NewPassword { get; set; }
 
     #endregion
 }
