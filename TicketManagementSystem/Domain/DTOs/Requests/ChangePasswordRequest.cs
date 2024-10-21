72d927d9a7bbeefd9ece877196d8f5fd7332a162
stephane.mouako <stephane.mouako@sioux.eu>
2024-10-21 10:50:30 +0200

    Updated change password request model

--- TicketManagementSystem/Domain/DTOs/Requests/ChangePasswordRequest.cs
+++ TicketManagementSystem/Domain/DTOs/Requests/ChangePasswordRequest.cs
@@ -8,15 +8,18 @@ public class ChangePasswordRequest
 
     [Required]
     [DataType(DataType.Password)]
-    [Compare(nameof(NewPassword))]
+    [Display(Name = "Confirm new password")]
+    [Compare(nameof(NewPassword), ErrorMessage = "Confirm new password does not match")]
     public string? ConfirmNewPassword { get; set; }
 
     [Required]
     [DataType(DataType.Password)]
+    [Display(Name = "Current password")]
     public string? CurrentPassword { get; set; }
 
     [Required]
     [DataType(DataType.Password)]
+    [Display(Name = "New password")]
     public string? NewPassword { get; set; }
 
     #endregion
